d548787ee44d38855cac256413e4fba901440da5
Arisbeth <arisbeth.hg@gmail.com>
2026-05-30 22:51:12 -0600

    feat: create lib directory structure & design systemt

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<section class="min-h-screen flex items-center justify-center bg-backing">
+	<div class="text-center">
+		<p class="uppercase tracking-[0.4em] text-sm text-fuchsia-400">Web Developer & UI Designer</p>
+
+		<h1 class="mt-6 text-7xl font-black leading-none">>_ building digital atmospheres</h1>
+
+		<!-- <p class="mt-8 max-w-xl mx-auto text-white/60">
+			Motion, interaction and modern frontend systems.
+		</p> -->
+		<p class="mt-8 max-w-xl mx-auto text-cyan-400">
+			Motion, interaction and modern frontend systems.
+		</p>
+	</div>
+</section>
